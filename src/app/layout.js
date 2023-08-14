3882bb37790718082e841e427bef151b8b8631ec
vahide <100282383+ashenhair@users.noreply.github.com>
2023-08-14 12:14:48 +0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,8 +22,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer/>
         </div>
-        </ThemeProvider>
-       
+        </ThemeProvider>       
         </body>
     </html>
   )
